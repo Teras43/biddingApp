9be21f6a03b7383cffb68ce71f1fe5c84ebe3ea3
Brandon Curtis <Brandcurtis43@gmail.com>
2020-10-14 13:53:18 -0600

    Added the localStorage function and added a loop to display the highest bid without clearing the array on each function call. Still working on localStorage.getItem

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 // Variables
 let all1Bids = [];
 let all2Bids = [];
+let totalBids = [];
 
 // Functions
 function addTotalBids() {
-    let totalBids = [];
     let mathMaxBids = Math.max(...all1Bids, ...all2Bids);
     totalBids.push(mathMaxBids);
-    console.log(totalBids);
-    document.getElementById('totalBid').innerHTML = '$ ' + totalBids;
+    let highestTotalBid = totalBids[0] || null;
+    function findHighest(totalBids) {
+        let number = null;
+        for (let i = 0; i < totalBids.length; i++) {
+            number = totalBids[i];
+            highestTotalBid = Math.max(highestTotalBid, number);
+            }
+            return highestTotalBid;
+        }
+    findHighest(totalBids);
+    document.getElementById('totalBid').innerHTML = '$ ' + highestTotalBid;
+    localStorageSave();
+}
+function localStorageSave() {
+    localStorage.setItem("totalBids", JSON.stringify(totalBids));
+}
+
+function localStorageGet() {
+    let dataGet = JSON.parse(localStorage.getItem("totalBids"));
+    totalBids.push(dataGet);
+    console.log("Grab successful!")
 }
 
 function user1Bids() {
@@ -24,3 +43,5 @@ function user2Bids() {
     document.getElementById("user2Bids").value = '';
     addTotalBids();
 }
+
+// window.onload = localStorageGet;
